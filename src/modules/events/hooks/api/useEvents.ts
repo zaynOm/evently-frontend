8e6d307000015050350e43d1807683ac194c91b3
zaynom <ouaziz0626@gmail.com>
2025-02-16 12:17:59 +0100

    feat(events): extend `useEvents` with join, leave, and participants API methods

--- src/modules/events/hooks/api/useEvents.ts
+++ src/modules/events/hooks/api/useEvents.ts
@@ -1,7 +1,9 @@
 import ApiRoutes from '@common/defs/api-routes';
-import { Event } from '@modules/events/defs/types';
-import useItems, { defaultOptions, UseItems, UseItemsOptions } from '@common/hooks/useItems';
+import { Event, EventParticipant } from '@modules/events/defs/types';
+import useItems, { defaultOptions, UseItemsHook, UseItemsOptions } from '@common/hooks/useItems';
 import { Dayjs } from 'dayjs';
+import useApi, { FetchApiOptions, ApiResponse } from '@common/hooks/useApi';
+import { Id } from '@common/defs/types';
 
 export interface CreateOneInput {
   title: string;
@@ -25,12 +27,50 @@ export interface UpdateOneInput {
 
 export type UpsertOneInput = CreateOneInput & UpdateOneInput;
 
-const useEvents: UseItems<Event, CreateOneInput, UpdateOneInput> = (
-  opts: UseItemsOptions = defaultOptions
-) => {
+export type UseEventsHook = UseItemsHook<Event, CreateOneInput, UpdateOneInput> & {
+  joinEvent: (id: Id, options?: FetchApiOptions) => Promise<ApiResponse<{ item: Event }>>;
+  leaveEvent: (id: Id, options?: FetchApiOptions) => Promise<ApiResponse<{ item: Event }>>;
+  participants: (
+    id: Id,
+    options?: FetchApiOptions
+  ) => Promise<ApiResponse<{ items: EventParticipant[] }>>;
+};
+
+const useEvents = (opts: UseItemsOptions = defaultOptions): UseEventsHook => {
   const apiRoutes = ApiRoutes.Events;
   const useItemsHook = useItems<Event, CreateOneInput, UpdateOneInput>(apiRoutes, opts);
-  return useItemsHook;
+  const fetchApi = useApi();
+
+  const joinEvent = async (id: Id, options?: FetchApiOptions) => {
+    const response = await fetchApi<{ item: Event }>(
+      ApiRoutes.Events.JoinEvent.replace('{id}', id.toString()),
+      { ...options, method: 'POST' }
+    );
+    return response;
+  };
+
+  const leaveEvent = async (id: Id, options?: FetchApiOptions) => {
+    const response = await fetchApi<{ item: Event }>(
+      ApiRoutes.Events.LeaveEvent.replace('{id}', id.toString()),
+      { ...options, method: 'POST' }
+    );
+    return response;
+  };
+
+  const participants = async (id: Id, options?: FetchApiOptions) => {
+    const response = await fetchApi<{ items: EventParticipant[] }>(
+      ApiRoutes.Events.Participants.replace('{id}', id.toString()),
+      options
+    );
+    return response;
+  };
+
+  return {
+    ...useItemsHook,
+    joinEvent,
+    leaveEvent,
+    participants,
+  };
 };
 
 export default useEvents;
