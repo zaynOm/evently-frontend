eceb46a648e434562e12f47fa472ed6603fc7843
zaynom <ouaziz0626@gmail.com>
2025-02-16 21:35:09 +0100

    feat(events): add card actions to the `EventGridItem` (view, edit, delete, join, leave)

--- src/modules/events/components/lib/EventGridItem.tsx
+++ src/modules/events/components/lib/EventGridItem.tsx
@@ -1,8 +1,16 @@
+import { useDialogContext } from '@common/contexts/DialogContext';
+import routes from '@common/defs/routes';
+import { CRUD_ACTION, Id } from '@common/defs/types';
+import useAuth from '@modules/auth/hooks/api/useAuth';
 import { Event } from '@modules/events/defs/types';
+import useEvents from '@modules/events/hooks/api/useEvents';
+import usePermissions from '@modules/permissions/hooks/usePermissions';
 import { AccessTime, CalendarToday, LocationOn } from '@mui/icons-material';
 import {
   Box,
+  Button,
   Card,
+  CardActions,
   CardContent,
   CardHeader,
   Chip,
@@ -11,14 +19,43 @@ import {
   Typography,
 } from '@mui/material';
 import dayjs from 'dayjs';
+import router from 'next/router';
 
 interface IEventGridProps {
   event: Event;
+  namespace: string;
+  fetchEvents: () => Promise<void>;
 }
 
 const EventGridItem = (props: IEventGridProps) => {
-  const { event } = props;
+  const { event, namespace, fetchEvents } = props;
+  const { deleteOne, joinEvent, leaveEvent } = useEvents();
+  const { openConfirmDialog } = useDialogContext();
+  const { can } = usePermissions();
+  const { user } = useAuth();
   const isFull = event.capacity === event.participantsCount;
+  const isOwner = user?.id === event.hostId;
+  const canJoin = !isOwner && !event.isParticipant && !isFull;
+  const canLeave = !isOwner && event.isParticipant;
+
+  const canUpdate = (id: Id) => {
+    return can(namespace, CRUD_ACTION.UPDATE) || can(namespace, CRUD_ACTION.UPDATE, id);
+  };
+
+  const canDelete = (id: Id) => {
+    return can(namespace, CRUD_ACTION.DELETE) || can(namespace, CRUD_ACTION.DELETE, id);
+  };
+
+  const handleJoin = async () => {
+    await joinEvent(event.id, { displayProgress: true, displaySuccess: true });
+    fetchEvents();
+  };
+
+  const handleLeave = async () => {
+    await leaveEvent(event.id, { displayProgress: true, displaySuccess: true });
+    fetchEvents();
+  };
+
   return (
     <Card
       sx={{
@@ -82,7 +119,75 @@ const EventGridItem = (props: IEventGridProps) => {
           </Box>
         </Stack>
       </CardContent>
+      <CardActions
+        sx={{
+          mt: 'auto',
+          p: 2,
+        }}
+      >
+        <Stack
+          direction={{ xs: 'column-reverse', sm: 'row' }}
+          justifyContent={{ xs: 'center', sm: 'space-between' }}
+          spacing={1}
+          flexGrow={1}
         >
+          {isOwner ? (
+            <Stack direction={{ xs: 'column', sm: 'row' }} spacing={1}>
+              {canUpdate(event.id) && (
+                <Button
+                  variant="outlined"
+                  color="inherit"
+                  onClick={() =>
+                    router.push(routes.Events.UpdateOne.replace('{id}', event.id.toString()))
+                  }
+                >
+                  Edit
+                </Button>
+              )}
+              {canDelete(event.id) && (
+                <Button
+                  variant="contained"
+                  color="error"
+                  onClick={() => {
+                    openConfirmDialog(
+                      'Delete',
+                      'Are you sure you want to delete this item? This action is irreversible.',
+                      () => {
+                        deleteOne(event.id, { displayProgress: true, displaySuccess: true });
+                        fetchEvents();
+                      },
+                      'Delete',
+                      'error'
+                    );
+                  }}
+                >
+                  Delete
+                </Button>
+              )}
+            </Stack>
+          ) : (
+            <>
+              {canJoin && (
+                <Button variant="contained" onClick={handleJoin}>
+                  Join
+                </Button>
+              )}
+              {canLeave && (
+                <Button variant="contained" onClick={handleLeave}>
+                  Leave
+                </Button>
+              )}
+            </>
+          )}
+          <Button
+            color="primary"
+            variant="outlined"
+            onClick={() => router.push(routes.Events.ReadOne.replace('{id}', event.id.toString()))}
+          >
+            View Details
+          </Button>
+        </Stack>
+      </CardActions>
     </Card>
   );
 };
